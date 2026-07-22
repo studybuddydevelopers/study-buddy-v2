24c0c2438eb62381bd8ff523d782516c509b9a30
Nick Efe Oni <neoni1@sheffield.ac.uk>
2026-07-22 22:17:15 +0100

    feat(config): add custom headers for caching
    This update introduces custom headers for specific assets.
    
    The Cache-Control header is set to public with a max-age of one year.
    
    This change aims to improve performance by leveraging browser caching.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  poweredByHeader: false,
+  async headers() {
+    return [
+      {
+        source: "/logo-icon.svg",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+      {
+        source: "/icons/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
