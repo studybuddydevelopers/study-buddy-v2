7c7fbbce1705c8bbe8e0c00011b1c7fe82f58fd4
Nick Efe Oni <neoni1@sheffield.ac.uk>
2026-09-08 05:14:07 +0100

    feat: add past questions page metadata

--- app/materials/past-questions/page.tsx
+++ app/materials/past-questions/page.tsx
@@ -1,5 +1,12 @@
+import type { Metadata } from "next";
 import { getMaterialsOverview } from "../materials-data";
 import PastQuestionsClient from "./PastQuestionsClient";
+
+export const metadata: Metadata = {
+  title: "Past Questions | Study Buddy",
+  description:
+    "Practise WAEC-style past questions by subject and strengthen the topics that need more work.",
+};
 
 export default async function PastQuestionsPage() {
   const subjects = await getMaterialsOverview();
